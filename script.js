3fdcd0879f74f56f1ae61fc84ee8dcfd7028e70b
Moses Mwangi <simp4mozzy@gmail.com>
2026-06-30 12:27:10 +0300

    fixed the hamburger btn

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var navLinks = document.getElementById("navLinks");
+var overlay = document.getElementById("overlay");
 
 function showMenu(){
     navLinks.style.right = "0";
+    overlay.classList.add("active");
 }
 
 function hideMenu(){
-    navLinks.style.right = "-200px"
+    navLinks.style.right = "-280px";
+    overlay.classList.remove("active");
 }
+
